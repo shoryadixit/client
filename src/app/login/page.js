5598254292b1643c7e1b3e0bb163a1711900b2b7
Shorya Dixit <shauryadt123@gmail.com>
2024-11-18 23:29:18 +0530

    Deploy FIxes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,10 +2,10 @@ import FormView from "@/view/FormView";
 
 export default function Page() {
   return (
-      <div>
-        <main className="flex w-full items-center justify-center flex-col py-10">
-          <FormView />
-        </main>
-      </div>
+    <div>
+      <main className="flex w-full items-center justify-center flex-col py-10">
+        <FormView />
+      </main>
+    </div>
   );
 }
